da4d924696a619b181638413abf5058d1dd7237c
Katerina Koleva <123803475+katerina5koleva@users.noreply.github.com>
2024-03-26 18:34:50 +0200

    Update EditIdea.cs

--- FlowingIdeasReal/FlowingIdeasReal/EditIdea.cs
+++ FlowingIdeasReal/FlowingIdeasReal/EditIdea.cs
@@ -13,6 +13,11 @@ using System.Windows.Forms;
 
 namespace FlowingIdeasApp
 {
+    
+    /// <summary>
+    /// This is a form in which the user can rewrite
+    /// the text of their idea.
+    /// </summary>
     public partial class EditIdea : Form
     {
         private int userId;
